d1bed5c7513aa477298bb06d293de4fbe5fbd6b9
Ayden Chance <achance6@jhu.edu>
2020-04-01 10:25:40 -0400

    created sign in layout

--- app/src/main/java/com/example/pinsplorer/SignInActivity.java
+++ app/src/main/java/com/example/pinsplorer/SignInActivity.java
@@ -3,12 +3,15 @@ package com.example.pinsplorer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = findViewById(R.id.signInToolbar);
+        toolbar.setTitle("PinSplorer");
         setContentView(R.layout.activity_sign_in);
     }
 }
