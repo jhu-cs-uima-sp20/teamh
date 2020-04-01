af0f9c67af015f16a86b9250533c10546c69ccfd
Ayden Chance <achance6@jhu.edu>
2020-04-01 14:03:32 -0400

    Display saved fragment upon creating main activity, allow navigation to EditProfileFragment from SavedFragment

--- app/src/main/java/com/example/pinsplorer/MainActivity.java
+++ app/src/main/java/com/example/pinsplorer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.pinsplorer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction trans = manager.beginTransaction();
+        Fragment frag = new SavedFragment();
+        trans.replace(R.id.fragment_cont, frag);
+        trans.addToBackStack(null);
+        trans.commit();
+        Button profile = findViewById(R.id.button);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction trans = manager.beginTransaction();
+                Fragment frag = new EditProfileFragment();
+                trans.replace(R.id.fragment_cont, frag);
+                trans.addToBackStack(null);
+                trans.commit();
+            }
+        });
+
     }
 
 }
