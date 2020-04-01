b1723135075bb044c2f2b2e1c18d67197410a96a
Ayden Chance <achance6@jhu.edu>
2020-04-01 13:20:55 -0400

    Finish sign in activity when user signs in added

--- app/src/main/java/com/example/pinsplorer/SignInActivity.java
+++ app/src/main/java/com/example/pinsplorer/SignInActivity.java
@@ -1,17 +1,30 @@
 package com.example.pinsplorer;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = findViewById(R.id.signInToolbar);
-        toolbar.setTitle("PinSplorer");
-        setContentView(R.layout.activity_sign_in);
+       setContentView(R.layout.activity_sign_in);
+        Button btn = findViewById(R.id.signInButton);
+        btn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(v.getContext(), MainActivity.class);
+               startActivity(intent);
+            }
+        });
     }
 }
