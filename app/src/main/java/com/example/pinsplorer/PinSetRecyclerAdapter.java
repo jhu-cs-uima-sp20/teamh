2f6e8336239898ed6d32bfdc6d4796d198e80f65
AJ Cariaga <ajcariaga16@gmail.com>
2020-04-04 23:00:35 -0400

    Worked on going from recycler view fragment to set view fragment

--- app/src/main/java/com/example/pinsplorer/PinSetRecyclerAdapter.java
+++ app/src/main/java/com/example/pinsplorer/PinSetRecyclerAdapter.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -38,7 +40,11 @@ public class PinSetRecyclerAdapter extends RecyclerView.Adapter<PinSetViewHolder
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //****TODO Take user to ViewSetFragment
+                AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                Fragment myFragment = new Fragment();
+                activity.getSupportFragmentManager()
+                        .beginTransaction().replace(R.id.fragment_view_set, myFragment)
+                        .addToBackStack(null).commit();
             }
         });
     }
