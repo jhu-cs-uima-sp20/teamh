3662d2f509d861619468bec5e7103c333c3f5d1a
unknown <adam.t.riyad@gmail.com>
2020-04-27 22:04:12 -0400

    Some database foundation and improved formatting on tablets

---
+++ app/src/main/java/com/example/pinsplorer/DBAdapter.java
@@ -0,0 +1,27 @@
+package com.example.pinsplorer;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBAdapter {/*
+    private SQLiteDatabase db;
+    private static DBAdapter dbInstance;
+    private DBHelper dbHelper;
+    private final Context context;
+
+
+    private static class DBHelper extends SQLiteOpenHelper {
+
+        private static final String DB_CREATE = "CREATE TABLE " + ITEMS_TABLE
+                + " (" + ITEM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ITEM_WHAT + " TEXT);";
+        public DBHelper (Context context, String name, SQLiteDatabase.CursorFactory fct, int version) {
+            super(context, name, fct, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase adb) { adb.execSQL(DB_CREATE); }
+
+
+    }
+*/
+}
